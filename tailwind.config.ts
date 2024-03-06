a1e4f513cf4ed214f66c5326d5269cf38b80234d
redh <mehdi.cheniki@gmail.com>
2024-03-05 21:42:38 -0500

    add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,24 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx,mdx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
-    theme: {
-        extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic':
-                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-            },
-        },
-    },
-    plugins: [],
-}
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		fontFamily: {
+			jetbrains: ['var(--font-jetbrains)', 'sans-serif'],
+			roundo: ['var(--font-roundo)', 'sans-serif']
+		},
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+			}
+		}
+	},
+	plugins: []
+};
 
-export default config
+export default config;
