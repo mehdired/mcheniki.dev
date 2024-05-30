a0284528bfeba69f4ddecfd3cc885534041b5050
redh <mehdi.cheniki@gmail.com>
2024-05-30 18:19:24 -0400

    wip

---
+++ components/StackParticules/ParticulesSecond.tsx
@@ -0,0 +1,134 @@
+'use client'
+
+import { useEffect, useMemo, useRef, useState } from 'react'
+import Particles, { initParticlesEngine } from '@tsparticles/react'
+import {
+    type Container,
+    type ISourceOptions,
+    MoveDirection,
+    OutMode,
+} from '@tsparticles/engine'
+import { loadSlim } from '@tsparticles/slim'
+
+export function ParticulesSecond() {
+    const [init, setInit] = useState(false)
+
+    // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+            // starting from v2 you can add only the features you need reducing the bundle size
+            //await loadAll(engine);
+            //await loadFull(engine);
+            await loadSlim(engine)
+            //await loadBasic(engine);
+        }).then(() => {
+            setInit(true)
+        })
+    }, [])
+
+    const particlesLoaded = async (container?: Container): Promise<void> => {
+        console.log(container)
+    }
+
+    const options: ISourceOptions = useMemo(
+        () => ({
+            background: {
+                color: {
+                    value: 'transparent',
+                },
+            },
+            fullScreen: false,
+            fpsLimit: 120,
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: false,
+                        mode: 'push',
+                    },
+                    onHover: {
+                        enable: false,
+                        mode: 'repulse',
+                    },
+                },
+                modes: {
+                    push: {
+                        quantity: 4,
+                    },
+                    repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                    },
+                },
+            },
+            particles: {
+                collisions: {
+                    mode: 'bounce',
+                    enable: true,
+                },
+                reduceDuplicates: true,
+                color: {
+                    value: '#ffffff',
+                },
+                links: {
+                    color: '#ffffff',
+                    distance: 3000,
+                    enable: true,
+                    opacity: 0.5,
+                    width: 1,
+                },
+                move: {
+                    direction: MoveDirection.none,
+                    enable: true,
+                    outModes: {
+                        default: OutMode.bounce,
+                    },
+                    random: false,
+                    speed: 0.1,
+                    straight: false,
+                },
+                number: {
+                    density: {
+                        enable: false,
+                    },
+                    value: 2,
+                },
+                shape: {
+                    type: 'image',
+                    options: {
+                        image: [
+                            {
+                                src: '/images/analytics.svg',
+                                width: 1,
+                                height: 1,
+                            },
+                            {
+                                src: '/images/other.svg',
+                                width: 1,
+                                height: 1,
+                            },
+                        ],
+                    },
+                },
+                size: {
+                    value: {
+                        min: 20,
+                        max: 25,
+                    },
+                },
+            },
+            detectRetina: true,
+        }),
+        []
+    )
+
+    return (
+        <Particles
+            id="second"
+            className="h-full w-full"
+            particlesLoaded={particlesLoaded}
+            options={options}
+        />
+    )
+}
--- components/StackSection/StackSection.tsx
+++ components/StackSection/StackSection.tsx
@@ -1,23 +1,36 @@
 import { Container } from '../Container'
-import { SmallCardStack } from '../StackCard/SmallStackCard'
-import { StackCard } from '../StackCard/StackCard'
-import IconWP from '@/svgs/wp.svg'
+import { ParticulesBack } from '../StackParticules/ParticulesBack'
+import { ParticulesFirst } from '../StackParticules/ParticulesFirst'
+import { ParticulesSecond } from '../StackParticules/ParticulesSecond'
 
 export function StackSection() {
     return (
-        <Container>
-            <header className="mb-32 lg:flex lg:justify-between lg:gap-48">
-                <h2 className="mb-32 text-48 font-semibold uppercase italic leading-[0.9] md:text-clamp-h2 lg:mb-0">
-                    <span className="block text-primary-500">Technical</span>
-                    <span className="text-stroke text-transparent">Stack</span>
-                </h2>
-                <p className="text-25 text-base-100">
-                    Faucibus cras ornare tristique adipiscing. Non morbi eros
-                    vivamus interdum orci viverra ultrices blandit.
-                </p>
-            </header>
-
-            <SmallCardStack />
-        </Container>
+        <>
+            <Container>
+                <header className="mb-32 lg:flex lg:justify-between lg:gap-48">
+                    <h2 className="mb-32 text-48 font-semibold uppercase italic leading-[0.9] md:text-clamp-h2 lg:mb-0">
+                        <span className="block text-primary-500">
+                            Technical
+                        </span>
+                        <span className="text-stroke text-transparent">
+                            Stack
+                        </span>
+                    </h2>
+                    <p className="text-25 text-base-100">
+                        Faucibus cras ornare tristique adipiscing. Non morbi
+                        eros vivamus interdum orci viverra ultrices blandit.
+                    </p>
+                </header>
+            </Container>
+            <div className="relative aspect-video max-h-[753px] min-h-[350px] ">
+                <div className="absolute left-0 top-0 -z-10 h-full w-full">
+                    <ParticulesBack />
+                </div>
+                <div className="radialParticule absolute left-0 top-0 z-10 h-full w-full"></div>
+                <div className="absolute left-1/2 top-1/2 z-20 h-[90%] w-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <ParticulesFirst />
+                </div>
+            </div>
+        </>
     )
 }
