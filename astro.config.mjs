7dd58e25ac48f974657d38ad22c98fcab8e6053f
redh <mehdi.cheniki@gmail.com>
2026-01-07 15:01:35 +0100

    update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import svgr from 'vite-plugin-svgr';
 import { loadEnv } from 'vite';
 
@@ -11,40 +10,37 @@ import sitemap from '@astrojs/sitemap';
 
 import node from '@astrojs/node';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-	experimental: {
-		svg: true,
-	},
-	site: PUBLIC_URL_WEBSITE ?? 'http://localhost:4321',
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		react({
-			include: ['**/react/*'],
-		}),
-		sitemap(),
-	],
-	adapter: node({
-		mode: 'standalone',
-	}),
-	vite: {
-		plugins: [
-			svgr({
-				include: '**/*.svg?react',
-				svgrOptions: {
-					plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
-					svgoConfig: {
-						plugins: [
-							'preset-default',
-							'removeTitle',
-							'removeDesc',
-							'removeDoctype',
-							'cleanupIds',
-						],
-					},
-				},
-			}),
-		],
-	},
+    experimental: {
+        svgo: true,
+    },
+    site: PUBLIC_URL_WEBSITE ?? 'http://localhost:4321',
+    integrations: [
+        react({
+            include: ['**/react/*'],
+        }),
+        sitemap(),
+    ],
+    adapter: node({
+        mode: 'standalone',
+    }),
+    vite: {
+        plugins: [svgr({
+            include: '**/*.svg?react',
+            svgrOptions: {
+                plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+                svgoConfig: {
+                    plugins: [
+                        'preset-default',
+                        'removeTitle',
+                        'removeDesc',
+                        'removeDoctype',
+                        'cleanupIds',
+                    ],
+                },
+            },
+        }), tailwindcss()],
+    },
 });
